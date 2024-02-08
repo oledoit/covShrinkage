0861714123a9c91bc9236a77afffb9f3d93e6dfb
oledoit <47441814+oledoit@users.noreply.github.com>
2024-02-08 12:00:20 +0700

    Update covCor.m
    Change terminology in comment to the definition of the variable called sample2.

--- covCor.m
+++ covCor.m
@@ -67,7 +67,7 @@ target(logical(eye(p)))=samplevar;
 
 % estimate the parameter that we call pi in Ledoit and Wolf (2003, JEF)
 Y2=Y.^2;
-sample2=(Y2'*Y2)./n; % sample covariance matrix of squared returns
+sample2=(Y2'*Y2)./n; % sample matrix of products of squared returns
 piMat=sample2-sample.^2;
 pihat=sum(sum(piMat));
 
